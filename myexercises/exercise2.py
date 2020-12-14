f8cacc5a5e571815a2bbb065f71b9b39923bd70c
evadevs <evaristenizey@gmail.com>
2020-12-15 00:05:06 +0200

    Added a practical exercise to exerice 2

--- myexercises/exercise2.py
+++ myexercises/exercise2.py
@@ -1,3 +1,4 @@
+print("\n""The output of the first practical exercise:""\n")
 def release_num(num):
     previousNum = 0
     for i in range(num):
@@ -6,3 +7,30 @@ def release_num(num):
         previousNum = i
 
 release_num(10)
+
+
+print("\n""======================================================" "\n")
+# same exercises: revision
+print("\n""The output of the second practical exercise:""\n")
+def print_numbers(numb):
+ # generate previous number and the sum between current and previous number
+    previous_number = 0
+    for x in range(numb):
+        sum = previous_number + x
+        print("The current number is: ",x, "\n""The previous number is: ",
+          previous_number, "\n""The sum is: ", sum)
+        previous_number = x
+print_numbers(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
