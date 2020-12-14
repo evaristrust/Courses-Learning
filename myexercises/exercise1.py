75e2b592ae1a27d9cad034ed3d45e509c5635f92
evadevs <evaristenizey@gmail.com>
2020-12-14 23:37:32 +0200

    Performed the same exercise in the exercise 1

--- myexercises/exercise1.py
+++ myexercises/exercise1.py
@@ -13,3 +13,19 @@ if product >=1000:
     print("The sum of ", num1, "and ", num2, "is ", sum)
 else:
     print("The product of ", num1, "and ", num2, "is ", product)
+
+
+# going to do the same exercises
+
+# Find two random numbers from 1 to 20, add them and if their sum is greater than 12, do product
+
+numa = random.randrange(1, 20)
+numb = random.randrange(1, 20)
+
+product = numa * numb
+sum = numa + numb
+
+if sum >= 12:
+    print("The sum ", numa, "and", numb, "is greater or equal to 12; thus the product is: ", product)
+else:
+    print("The sum is less than 12, and it is: ", sum, ": addition of ", numa, "and", numb)
