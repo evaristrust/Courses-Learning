1da942fad49cced8bc187e6602773258a25e2f61
evadevs <evaristenizey@gmail.com>
2020-12-16 23:25:10 +0200

    Continued the game codes

--- FirstProgram/roleplay/game.py
+++ FirstProgram/roleplay/game.py
@@ -1,4 +1,5 @@
-from roleplay.RPG import person, colors
+from rpg import person, colors
+
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Thunder", "cost": 12, "dmg": 40},
          {"name": "Flood", "cost": 20, "dmg": 50}]
@@ -7,19 +8,67 @@ enemy = person(1200, 65, 45, 25, magic)
 running = True
 i = 0
 # print out enemy attacks with setting colors
-print(colors.color1 + colors.color3 + "AN ENEMY ATTACKS!" + colors.color4)
+print(colors.FAIL + colors.BOLD + "AN ENEMY ATTACKS!" + colors.ENDC)
 while running:
-    print("===================")
-   # player.choose_action()
+    print("===================" + colors.OKBLUE)
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+
+    if index == 0:
+       dmg = player.generate_damage()
+       enemy.take_damge(dmg)
+       print("You attacked for ", dmg, "points of damage. Enemy hp:", enemy.get_hp())
+    elif index == 1:
+        player.choose_magic()
+        magic_choice = int(input("Choose magic")) - 1
+        magic_dmg = player.generate_spell_damge(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
+
+        current_mp = player.get_mp()
+        if cost > current_mp:
+            print (colors.FAIL + "\n Not enough MP \n" + colors.ENDC)
+            continue
+        player.reduce_mp(cost)
+
+        # Tell enemy to take damage
+
+        enemy.take_damge(magic_dmg)
+        print(colors.OKBLUE + "\n" + spell + "Deals", str(magic_dmg), "points of damage" + colors.ENDC)
+
+
+    enemy_choice = 1
+    enemy_dmg = enemy.generate_damage()
+    player.take_damge(enemy_dmg)
+    print("Enemy attacked for ", enemy_dmg)
+
+    print("================================")
+    print("Enemy HP: ", colors.FAIL +str(enemy.get_hp()) + "/" + str(enemy.get_maxhp()) + colors.ENDC)
+    print("Your HP: ", colors.OKBLUE + str(player.get_hp()) + "/" +str(player.get_maxhp()) + colors.ENDC + "\n")
+
+    if enemy.get_hp() == 0:
+        print(colors.OKBLUE + "You win" + colors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(colors.FAIL + "Enemy defeated you" +colors.ENDC)
+        running = False
+
+# N.B : Just advanced on the course and will come back to playing this funny game!!!!
+# Will resume at Healing our player
+# There is still an error on the choose magic! 2
+"""
     player.choose_magic()
-    choice = input("Choose Action")
-   # print("You choose", choice)
+    choice = input("Choose Action: ")
+    print("You choose", choice)
     index = int(choice) - 1
     print("You choose", player.get_spell_name(int(choice)))
     running = False
-    # i += 1
-"""
+    i += 1
+    
+    
 print(player.generate_damage())
 print(player.generate_damage())
 print(player.generate_damage())
+
 """
--- FirstProgram/roleplay/rpg.py
+++ FirstProgram/roleplay/rpg.py
@@ -1,10 +1,15 @@
 import random
-#creating a colors class
+
+# creating a colors class
 class colors:
-    color1 = "\033[0;37;40m"
-    color2 = "\033[2;37;40m"
-    color3 = "\033[1;37;40m"
-    color4 = "\033[3;37;40m"
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    WARNING = "\033[92m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
 
 # create person class
 class person:
@@ -13,38 +18,49 @@ class person:
         self.hp = hp
         self.maxmp = mp
         self.mp = mp
-        self.atkh = atk + 10
         self.atkl = atk - 10
+        self.atkh = atk + 10
         self.df = df
         self.magic = magic
         self.actions = ["Attack", "Magic"]
+
     # let's generate damage
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
-    def generate_spell_damge(self):
-        mgl = self.magic[1]["dmg"] - 5
-        mgh = self.magic[1]["dmg"] + 5
-        return random.randrange(mgl,mgh)
+
+    def generate_spell_damge(self, i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+
     # Create a function for a player or enemy to take damage
     def take_damge(self, dmg):
         self.hp = dmg
         if self.hp < 0:
             self.hp = 0
         return self.hp
+
     def get_hp(self):
         return self.hp
+
     def get_maxhp(self):
         return self.maxhp
+
     def get_maxmp(self):
         return self.maxmp
+
     def get_mp(self):
         return self.mp
+
     def reduce_mp(self, cost):
         self.mp = cost
-    def get_spell_name(self):
-        return self.magic[1]["name"]
+
+    def get_spell_name(self, i):
+        return self.magic[i]["name"]
+
     def get_spell_mp_cost(self):
-        return self.magic[1]["cost"]
+        return self.magic[i]["cost"]
+
     # two more methods
 
     # choose action and choose magic
@@ -52,16 +68,12 @@ class person:
         i = 1
         print("Actions")
         for item in self.actions:
-            print(str(1) + ":", item)
+            print(str(i) + ":", item)
             i += 1
+
     def choose_magic(self):
         i = 1
         print("Magic")
         for spell in self.magic:
-            print(str(1) + ":", spell["name"], "(cost:", str(spell["dmg"]) + ")")
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["dmg"]) + ")")
             i += 1
-
-
-
-
-
