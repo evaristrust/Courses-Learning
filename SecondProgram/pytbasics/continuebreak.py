0c32ab355744c2d64afe41cad56d0b31c5c2df4f
evadevs <evaristenizey@gmail.com>
2020-12-19 08:39:17 +0200

    added a practical break exercise

--- SecondProgram/pytbasics/continuebreak.py
+++ SecondProgram/pytbasics/continuebreak.py
@@ -17,17 +17,47 @@
 # for example, you are going to buy different types of beers except primus
 # the seller says everything including primus but you don't wanna it
 # do that coding
+#
+# drinks = ["mtzing", "skol", "panache", "amstel", "primus" ]
+#
+# let_me_drink = ""
+#
+# for item in drinks:
+#     if item == "primus":
+#         let_me_drink = item
+#         break
+# else:
+#     print("Give me all of those")
+#
+# if let_me_drink == "primus":
+#     print("Can I have everything without primus please?")
 
-drinks = ["mtzing", "skol", "panache", "primus"]
 
-let_me_drink = ""
 
-for item in drinks:
-    if item == "primus":
-        let_me_drink = item
+
+
+
+
+
+
+
+
+
+
+
+watch_games = ["Football","Basket", "Box","Tennis" , "Volleyball"]
+
+funny_game = ""
+
+for item in watch_games:
+    if item == "Box":
+        funny_game = item
         break
 else:
-    print("Give me all of those")
+    print("I will watch all of these games")
 
-if let_me_drink == "primus":
-    print("Can I have everything without primus please?")
+if funny_game == "Box":
+    print("Can I have tickets for all other games excluding Box?")
+
+
+
