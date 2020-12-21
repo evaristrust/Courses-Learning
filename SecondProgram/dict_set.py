f6b7a0e4b64008fdbc39ec352102b3ce714d66a8
evadevs <evaristenizey@gmail.com>
2020-12-21 16:48:29 +0200

    added a logic of stopping the player after 5 trials

--- SecondProgram/dict_set.py
+++ SecondProgram/dict_set.py
@@ -7,6 +7,7 @@ print(champions["epl"]) # we access the content by not using index, we use keys
 
 champions["ligue1"] = "PSG, Marseille, Lille"
 champions["epl"] = "Manchester city, Liverpool, chelsea"
+champions["bundesliga"] = "Bayern, Dortmund, Frankfurt"
 print(champions)
 
 # what if I want to delete an item from the dict
@@ -26,6 +27,18 @@ print(champions)
 
 # Let's ask a user to put check the top three teams in this championships
 
+# print("Type epl, ligue1, laliga, or serie_a to check the top"
+#       "teams in the chosen ligue\nType 'quit' to exit")
+#
+# while True:
+#     check_teams = input("Please Enter a name of a ligue: ")
+#     if check_teams == "quit":
+#         break
+#     description = champions.get(check_teams)
+#     print(description)
+# you can easily see that if the check_teams is wrong, they print none
+# but you can put the condition too as below:
+
 print("Type epl, ligue1, laliga, or serie_a to check the top"
       "teams in the chosen ligue\nType 'quit' to exit")
 
@@ -33,5 +46,8 @@ while True:
     check_teams = input("Please Enter a name of a ligue: ")
     if check_teams == "quit":
         break
-    description = champions.get(check_teams)
-    print(description)
+    if check_teams in champions:
+        description = champions.get(check_teams)
+        print(description)
+    else:
+        print("We don't have {}".format(check_teams))
--- SecondProgram/pytbasics/test.py
+++ SecondProgram/pytbasics/test.py
@@ -4,14 +4,15 @@
 # if they get it, break the loop
 
 import random
-
+# initiate variables, max, guess, trials, and the range for the number guesses
 print("Choose a number between 1 and 20")
 max_number = 20
 number = random.randint(1,max_number)
 guess = 0
+trials = 1
 while guess != number:
     guess = int(input())
-    if guess == 0 or guess > max_number:
+    if guess == 0 or guess > max_number or trials > 5:
         break
     if guess < number:
         print("choose higher")
@@ -19,6 +20,8 @@ while guess != number:
         print("choose lower")
     else:
         print("You chose the right number")
+    trials += 1
+
 
 # Support the max_number is 10000! and I want to stop the loop at the player's 10th guess
 # Which coding can i do to make this wish?? I don't get how i can do it
