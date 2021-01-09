9b423c6731a0ff60db399037d5159faf7ba8a10b
evadevs <evaristenizey@gmail.com>
2021-01-09 20:29:14 +0200

    updated challenge1

--- SecondProgram/intro_list_range_tuples/challenge1.py
+++ SecondProgram/intro_list_range_tuples/challenge1.py
@@ -17,3 +17,24 @@ for items in menu:
     if not "rice" in items:
         for value in items:
             print(value)
+
+# Learn how to remove duplicates in a list
+
+my_colors = ["black", "white", "red","yellow", "purple", "white",
+             "black", "white","black", "orange", "purple"]
+
+# convert it back to the dictionaries and then back to the list
+
+remove_duplicates = list(dict.fromkeys(my_colors))
+
+print("The list without duplicates is {}".format(remove_duplicates))
+
+# doing this using function
+
+def my_list(x):
+    return list(dict.fromkeys(x))
+
+original_list = my_list(["pens", "books", "books", "notebook", "pens", "notebook"])
+
+print("Your child will need {}: ".format(original_list))
+
