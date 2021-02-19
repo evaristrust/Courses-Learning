a6a2ba72beb7bba49661395c0eecadd37a3aeb9a
evadevs <evaristenizey@gmail.com>
2021-02-19 14:20:13 +0200

    POlymorphism with functions and objects

--- SecondProgram/OOP/Polymorphism.py
+++ SecondProgram/OOP/Polymorphism.py
@@ -41,3 +41,59 @@ for country in (rda, gmn):
     country.languages()
     country.type()
     country.location()
+
+print('##' * 40)
+#Poly with inheritance
+class Taylor:
+    def album(self):
+        print('Reputation is dope')
+    def track(self):
+        print('Gergeous is a top-notch')
+
+class Swift(Taylor):
+    def album(self):
+        print('Evermore is dope recent album')
+    def track(self):
+        print('Champagne problems is a my favorite in the album')
+
+obj1 = Taylor()
+obj2 = Swift()
+for music in (obj1, obj2):
+    music.album()
+    music.track()
+
+#Polymorphism with a Function and objects
+print('**' * 40)
+class USA(object):
+    def capital(self):
+        print('Washington is the capital city of Rwanda')
+    def languages(self):
+        print('English is the mother tongue and spoken with everyone')
+    def type(self):
+        print('USA is a developed country')
+    def location(self):
+        print('It is located in the North America')
+
+class Italy(object):
+    def capital(self):
+        print('Roma is the capital city of Germany')
+    def languages(self):
+        print('Italian is the mother tongue and spoken with everyone')
+    def type(self):
+        print('Italy is a developed country')
+    def location(self):
+        print('It is located in the EU/Shengeni')
+
+#Creating a function here to drive codes
+def func(obj):
+    obj.capital()
+    obj.languages()
+    obj.type()
+    obj.location()
+
+usa_obj = USA()
+italy_obj = Italy()
+
+#Invoking the function here:
+func(usa_obj)
+func(italy_obj)
