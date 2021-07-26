8a4a4e2244cbf38a368d2ad2edf10f8742cd0f44
evadevs <evaristenizey@gmail.com>
2021-07-26 10:48:37 +0200

    grossnet

--- SecondProgram/pytbasics/CHALLENGES/personaltraining/grossNet.py
+++ SecondProgram/pytbasics/CHALLENGES/personaltraining/grossNet.py
@@ -1,5 +1,5 @@
 #ask a user their gross salary to output their net salary (Rwandan)
-#try and see if you can compute their gross pay from their net pay
+#ask a user their gross also to get their pay as you earn (PAYE) (rwanda)
 class Net:
 
     def __init__(self, balance):
@@ -17,13 +17,26 @@ class Net:
         else:
             print('Your Net Salary is {}'.format(round(p - ((p-med)*0.3 + (70000*0.2) + s))))
 
+    def getPaye(self):
+            low = 30000 # lower earning (zero taxed)
+            med = 100000 # medium earning (20% taxed)
+            p = int(input('What\'s your gross salary? ')) # the amount in gross
+            if p <= low:
+                print('Your PAYE is 0') #rounding to get whole number Net pay
+            elif p <= med:
+                print('Your PAYE is {}'.format((p-low)*0.2))
+            else:
+                print('Your Net Salary is {}'.format((p-med)*0.3 + (70000*0.2)))
+
 if __name__ == '__main__':
     check = Net(0)
 
 while check:
-    user = input('Wanna check your net or your gross? ')
+    user = input('Wanna check your PAYE or NET Salary? ')
     if user.upper() == 'NET':
         check.getNet()
+    elif user.lower() == 'paye':
+        check.getPaye()
     else:
         print('sorry, we don\'t have that option')
         break
